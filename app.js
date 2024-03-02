2d59f47be40241d429718d5c32e29f9dd524b68e
weltonDev <welton.developer95@gmail.com>
2024-03-02 17:10:50 -0300

    teste

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 console.log('hello world');
+
+const teste = document.querySelector('#teste');
+
+const visibiltyMessage = ()=> {
+  
+}
+
+setTimeout(() => {
+  console.log('teste')
+  teste.style.display = 'none'
+}, 1000);
